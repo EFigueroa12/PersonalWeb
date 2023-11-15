88db09281f4bae456de538bdf98f788fa8ba0ce6
EFigueroa12 <efigueroa6912@gmail.com>
2023-11-15 14:30:57 -0800

    where is this

---
+++ EFigueroaWeb/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <p> TEST </p>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
